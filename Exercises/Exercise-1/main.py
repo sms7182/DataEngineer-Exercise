e2e7e64a251d9f47d3d78a5866a4f8f7bafa9420
FatalError <sms7182@gmail.com>
2024-12-31 11:39:45 +0330

    first commit for Exercise1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,12 @@
+import sys, os
+import zipfile
 import requests
+from io import BytesIO
+import aiohttp
+import asyncio
+import csv
+from pathlib import Path
+from contextlib import closing
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,9 +19,32 @@ download_uris = [
 ]
 
 
+
 def main():
+    
+    Path("./downloads").mkdir(parents=True,exist_ok=True)
+    for url in download_uris:
+        try:
+            response=requests.get(url)
+            fname=url.split("/")[-1]
+            file_path="./downloads/"+fname
+            with open(file_path,'wb') as wz:
+                wz.write(response.content)
+                zip_file=zipfile.ZipFile(file_path)
+                files=zip_file.namelist()
+                csv_file_path = "./downloads/"+files[0]
+                with open(csv_file_path,'wb') as of:
+                    of.write(zip_file.read(files[0]))
+                os.remove(file_path)
+                            
+
+        except Exception as e:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            print(e)
     # your code here
-    pass
+    
+
 
 
 if __name__ == "__main__":
